a7b1a3eed897ab82a9f2b4f47cc0a4f98bd2a9c3
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2019-03-02 12:09:56 +0100

    Add documentation and improve readability

--- wrappingshell.py
+++ wrappingshell.py
@@ -1,3 +1,6 @@
+"""Wrapper to get an interactive shell on nc-based call backs
+
+Also provide some zos and ssh pivoting specific commands"""
 import argparse
 import sys
 from cmd import Cmd
@@ -5,6 +8,12 @@ from zosutils import IOWrapper
 
 
 class WrappingShell(Cmd):
+    """Use IOWrapper to get a trans-coded shell interface.
+
+    This class can optionally manage synchronous shell command by adding a so-called
+    command separator which helps the receiver to sync and detect when a remote
+    command ends.
+    """
     TERMINATOR_STRING = 'previous_command_ended_Gcv9we7WYtGqRR7gEPRaw9ZquqZUZurj'
     COMMAND_TERMINATOR = 'echo ' + TERMINATOR_STRING + '\n'
 
@@ -26,50 +35,61 @@ class WrappingShell(Cmd):
             self.wrapper.start_readout()
 
     def preloop(self):
-        # Set up command completion
+        """Set up command completion"""
         Cmd.preloop(self)
         if self.synchronous_commands:
             print('Waiting for ' + self.name + ' activation')
             self.wrapper.synchronous_thread_out(self.termination_string)
 
     def encode_and_send(self, args):
+        """Encode a shell command and send it to the remote host"""
         data = args.encode()
         self.wrapper.writein(self.wrapper.to_ebcdic(data))
         if self.synchronous_commands:
             sys.stdout.write(self.wrapper.synchronous_thread_out(self.termination_string))
 
     def prepare_command(self, args):
-        # Detect if we got a command to run in background
-        if args.split()[-1] == '&' or args.endswith('&'):
+        """Prepare a command to send to the shell.
+
+        The command separator changes depending on two factors:
+         - having a synchronous shell
+         - foreground vs background activity"""
+        if args.split()[-1] == '&' or args.endswith('&') or self.termination_command == '':
             sync_character = ''
         else:
             sync_character = ';'
         return args + sync_character + self.termination_command
 
     def default(self, args):
+        """Send the command to the remote host by default"""
         self.poll_subprocess()
         command = self.prepare_command(args)
         self.encode_and_send(command)
 
     def do_tsocmd(self, args):
+        """Get autocomplete for tsocmd when running on zOS"""
         real_args = 'tsocmd ' + args
         self.default(real_args)
 
     def get_name(self):
+        """Get shell name"""
         return self.name
 
     def emptyline(self):
+        """Check if the subprocess (remote shell) is alive on empty lines"""
         self.poll_subprocess()
 
     def postcmd(self, stop, args):
+        """Set the stop flag for the Subprocess (remote shell)"""
         return self.stop
 
     def terminate(self):
+        """Terminate the subprocess (remote shell)"""
         self.wrapper.terminate()
         self.stop = True
 
     def do_exit(self, args=None):
-        """Quits the shell"""
+        """Quit the shell"""
         confirm = input("This will terminate the connection to " +
                         self.get_name() + ", are you sure? (y/N) ")
         try:
@@ -81,10 +101,11 @@ class WrappingShell(Cmd):
             return
 
     def do_EOF(self, args=None):
-        """Quits the shell"""
+        """Quit the shell on EOF (Ctrl+D on *nix)"""
         self.do_exit()
 
     def poll_subprocess(self):
+        """Check if the subprocess (remote shell) is alive and terminates if not"""
         if self.wrapper.poll() is not None:
             print('Subprocess terminated, exiting')
             self.terminate()
