b4c832c7487800ad656dbc1d6333dd652fc17bb9
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2019-03-03 16:30:28 +0100

    Remove unused methods, add documentation

--- zosutils/ftp.py
+++ zosutils/ftp.py
@@ -1,3 +1,4 @@
+"""FTP library exploiting the peculiar z/OS features"""
 import ftplib
 import io
 import string
@@ -5,6 +6,7 @@ import random
 
 
 class FTP():
+    """Object to interact with the mainframe FTP capabilities"""
     DEFAULT_TEMPORARY_PATH = '/tmp'
 
     def __init__(self, hostname, username, password, port=21, passive=True):
@@ -16,28 +18,17 @@ class FTP():
         self.ftp = ftplib.FTP()
 
     def connect(self):
+        """Establish a connection"""
         # Create the connection
         self.ftp.connect(self.hostname, self.port)
         self.ftp.login(self.username, self.password)
         if self.passive:
             self.ftp.set_pasv(True)
 
-    def getds(self, dsname, outfile=''):
-        if outfile == '':
-            outfile = dsname
-        # Get the dataset
-        fp = open(outfile, 'wb')
-        self.ftp.retrbinary('RETR %s' % dsname, fp.write)
-        fp.close()
-
-    def getdslist(self, outfile):
-        fp = open(outfile, 'w')
-        self.ftp.retrlines("LIST '*.*'", writeln + '\n')
-        fp.close()
-
     def gen_random_filename(self, path=DEFAULT_TEMPORARY_PATH,
                             chars=string.ascii_lowercase + string.digits,
                             length=16):
+        """Generate a random filename and check if the file already exists"""
         filename = path + '/' + ''.join(random.choice(chars) for x in range(length))
         found_files = []
         while True:
@@ -49,15 +40,18 @@ class FTP():
         return filename
 
     def upload_string_as_file(self, bytestring, remotefile, permissions='600'):
+        """Upload a string into a file"""
         fp = io.BytesIO(bytestring)
         self.ftp.storlines('STOR ' + remotefile, fp)
         self.ftp.sendcmd('SITE CHMOD ' + permissions + ' ' + remotefile)
         fp.close()
 
     def run_jcl(self, jcl):
+        """Run a string as a JCL batch"""
         self.ftp.sendcmd('site filetype=jes')
         jclfp = io.BytesIO(jcl.encode())
         self.ftp.storlines('STOR FTPJOB', jclfp)
 
     def close(self):
+        """Close the connection"""
         self.ftp.close()
