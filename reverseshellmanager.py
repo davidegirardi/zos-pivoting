e1479d8cae70574f6aac0574cab4aadb675cb695
davidegirardi <16451191+davidegirardi@users.noreply.github.com>
2019-03-09 21:25:01 +0100

    Add sshd server config generation

--- reverseshellmanager.py
+++ reverseshellmanager.py
@@ -4,7 +4,15 @@ Also provide some z/OS and ssh pivoting specific commands"""
 import argparse
 from string import Template
 from zosutils import WrappingShell
+from sshutils import ssh_utils
 
+class ArgumentParserError(Exception):
+    """Custom exception for argparse"""
+
+class CmdArgumentParser(argparse.ArgumentParser):
+    """Extend argparse.ArgumentParser with error management"""
+    def error(self, message):
+        raise ArgumentParserError(message)
 
 class ReverseShellManager(WrappingShell):
     """Extend zosutils.WrappingShell with post-exploitation specific commands"""
@@ -43,6 +51,54 @@ EXAMPLE:
         print('Running in background: ' + run_ssh_command)
         self.default(run_ssh_command)
 
+    def __sshdgen_parser(self):
+        """Use argparse to generate the help for do__sshdgen"""
+        parser = CmdArgumentParser(
+            'Generates a basic sshd configuration file',
+            add_help=False,
+            )
+        parser.add_argument(
+            '-p', '--port', help='sshd listening port', default='22222')
+        parser.add_argument(
+            '-l', '--listen', help='sshd listen address', default='127.0.0.1')
+        parser.add_argument(
+            '-r', '--rsakeypath', help='path to the rsa host key', required=True)
+        parser.add_argument(
+            '-a', '--authorizedkeys', help='path of the authorized_keys file',
+            required=True)
+        parser.add_argument(
+            '-u', '--username', help='grant login access to this user',
+            required=True)
+        parser.add_argument(
+            '-s', '--strictmode',
+            help='set to no if the authorized_keys file is in a public location',
+            choices=['yes', 'no'],
+            default='yes',
+            )
+        return parser
+
+    def help__sshdgen(self):
+        """Redirects the help to the __sshdgen_parser print help"""
+        self.__sshdgen_parser().print_help()
+
+    def do__sshdgen(self, args):
+        """Create a sshd server config"""
+        parser = self.__sshdgen_parser()
+        try:
+            options = parser.parse_args(args.split())
+        except ArgumentParserError:
+            parser.print_help()
+            return
+        config = ssh_utils.gen_sshd_config(
+            options.port,
+            options.listen,
+            options.rsakeypath,
+            options.authorizedkeys,
+            options.username,
+            options.strictmode,
+            )
+        print(config)
+
 if __name__ == '__main__':
     # Argument parsing
     parser = argparse.ArgumentParser(
@@ -50,21 +106,34 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--encoding', type=str, help='set the encoding (default cp037)',
                         default='cp037')
     parser.add_argument('-n', '--name', type=str, help='name', default='shell')
-    parser.add_argument('-a', '--asyncio', help='asynchronous IO (like netcat)',
-                        default=True, action='store_false')
-    subparsers = parser.add_subparsers(title='subcommand',
-                                       description='external command to wrap',
-                                       help='program to execute')
+    parser.add_argument(
+        '-a',
+        '--asyncio',
+        help='asynchronous IO (like netcat)',
+        default=True,
+        action='store_false',
+        )
+    subparsers = parser.add_subparsers(
+        title='subcommand',
+        description='external command to wrap',
+        help='program to execute',
+        )
     command_parser = subparsers.add_parser('cmd', add_help=False)
 
     local_arguments, wrapped_command = parser.parse_known_args()
     wrapping_encoding = local_arguments.encoding
 
     if local_arguments.name is None:
-        shell = ReverseShellManager(wrapped_command, wrapping_encoding,
-                                    sync_stdout=local_arguments.asyncio)
+        shell = ReverseShellManager(
+            wrapped_command,
+            wrapping_encoding,
+            sync_stdout=local_arguments.asyncio
+            )
     else:
-        shell = ReverseShellManager(wrapped_command, wrapping_encoding,
-                                    name=local_arguments.name,
-                                    sync_stdout=local_arguments.asyncio)
+        shell = ReverseShellManager(
+            wrapped_command,
+            wrapping_encoding,
+            name=local_arguments.name,
+            sync_stdout=local_arguments.asyncio,
+            )
     shell.cmdloop()
--- sshutils/ssh_utils.py
+++ sshutils/ssh_utils.py
@@ -1,9 +1,22 @@
 """SSH key management and conversion"""
 import subprocess
 import tempfile
-
+from string import Template
 
 DEFAULT_KEYLENGTH = '1024'
+
+SSHD_CONFIG_TEMPLATE = '''
+# ssh-keygen -t rsa -f rsatest -b 2048 -N ''
+Port ${SERVER_PORT}
+ListenAddress ${LISTEN_ADDRESS}
+HostKey ${RSA_SERVER_KEY}
+AuthorizedKeysFile ${AUTHORIZED_KEYS_FILE}
+AllowUsers ${USERNAME}
+PasswordAuthentication no
+# Change to yes if the AuthorizedKeysFile is in /tmp or something similar
+StrictModes ${STRICT_MODES}
+Subsystem   sftp    /usr/lib/ssh/sftp-server
+'''
 
 def gen_rsa_key(keylength=1024):
     """Generate RSA private and public keys"""
@@ -51,3 +64,16 @@ def pub_rsa_to_openssh(key):
     opensshpubkey = proc.stdout
     fp.close()
     return opensshpubkey
+
+def gen_sshd_config(port, ListenAddress, RSAHostKeyPath, AuthorizedKeysFile,
+                    AllowUsers, StrictModes):
+    """Generate a minimal sshd_config file"""
+    config = Template(SSHD_CONFIG_TEMPLATE).substitute(
+        SERVER_PORT=port,
+        LISTEN_ADDRESS=ListenAddress,
+        RSA_SERVER_KEY=RSAHostKeyPath,
+        AUTHORIZED_KEYS_FILE=AuthorizedKeysFile,
+        USERNAME=AllowUsers,
+        STRICT_MODES=StrictModes,
+        )
+    return config
